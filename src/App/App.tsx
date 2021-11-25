e5510cf9ee0b8a36bc94756953afbe4bc90fe422
Kevin Tivert <kevintivert@MacBook-Pro.local>
2021-11-25 17:08:17 +0200

    -mnavbar done

---
+++ src/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Styles/App/App.css';
+import Navbar from '../Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <Navbar/>
+      </section>
+    </div>
+  );
+}
+
+export default App;
