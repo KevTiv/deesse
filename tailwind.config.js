02966c6be5121cbdc57cc16041007f0709b896c0
Kevin Tivert <kevintivert@MacBook-Pro.local>
2021-11-25 09:37:07 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        oldNewsPaper:{ 
+          DEFAULT: '#E0C9A6',
+        },
+        revampCar:{
+          light: '#637DA7',
+          dark: '3E5580',
+        },
+
+        oldNewsPaperFontColor:{
+          DEFAULT: '#614B2B',
+        },
+        revampCarFontColor:{
+          DEFAULT: '#FFFFFF',
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
